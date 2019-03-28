e15338a83075bdf1392dfca11afbbd5246732747
taracdia <36205314+taracdia@users.noreply.github.com>
2019-03-28 18:30:44 -0500

    from inside AS

--- app/src/main/java/com/taracdia/minesweeper/MainActivity.java
+++ app/src/main/java/com/taracdia/minesweeper/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     TextView flagsLeftTextView;
     ImageButton imageButton;
 
+    //fake edit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
